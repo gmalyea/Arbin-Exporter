d5e136638151dcdbdfef435ef4651d010e3c1358
garret <garret@garret.org>
2020-09-02 17:24:05 -0400

    Export by Test_ID
    Can now specify a specific Test_ID to export on the command line

--- list_tests.py
+++ list_tests.py
@@ -1,4 +1,3 @@
-import sys
 import pyodbc
 import pandas as pd
 from ArbinDatabase import ArbinDatabase
@@ -6,26 +5,10 @@ from ArbinTestItem import ArbinTestItem
 from ArbinExport import ArbinExport
 
 # =============================================================================
-# Export
+# List Tests
 # -----------------------------------------------------------------------------
-# Usage
-# -----------------------------------------------------------------------------
-# To export all tests:
-# >python export.py
-#
-# To export specific Test_IDs:
-# >python export.py Test_ID1 Test_ID2 Test_ID3 etc...
-#
 # Issues
 # -----------------------------------------------------------------------------
-# - Tests are only on one channel?
-# - Data is only in one database?
-# - drop incomplete rows?
-# - single channel aux data? - Test #46
-# - Check for Max datapoints - do this on the fly during export?
-# - Power might be calculated
-# - Test Name: File Name
-# - Convert Times - order of date
 #
 # =============================================================================
 
@@ -42,25 +25,19 @@ PASSWORD = 'Garret2020'
 arbinDatabase = ArbinDatabase( SERVER, USERNAME, PASSWORD )
 
 
-# Get Tests and Process
+# Get Tests and Print
 # -----------------------------------------------------------------------------
-if sys.argv[1] != "":
-    tests = pd.DataFrame(sys.argv[1:], columns = ['Test_ID']) 
-else:
-    tests = arbinDatabase.test_list()
+tests = arbinDatabase.test_list()
+
+print( "Test List" )
+print( "-----------------------" )
 
 for index, row in tests.iterrows():
     # Get Test ID
     testID = row['Test_ID']
     
     # Process Test
-    print( "Processing Test ID: " + str(testID) )
-    arbinTest = ArbinTestItem( testID, arbinDatabase )
-    
-    # Export
-    arbinExport = ArbinExport( arbinTest )
-    arbinExport.save_workbook( "Output/" )
-    
-    
+    print( str(testID) )
+
 
 print( "END" )
